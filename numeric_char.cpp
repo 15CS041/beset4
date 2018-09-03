31e60c2552d08e0cefdac2f37a88f3bb4a544f7d
15CS041 <rajaudhayaap1998@gmail.com>
2018-09-03 15:30:06 +0530

    Create numeric_char.cpp

---
+++ numeric_char.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char str[50],i,c=0;
+printf("enter the word");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+if((str[i]>='0')&&(str[i]<='9'))
+{
+c++;
+}
+}
+printf("%d",c);
+}
+
